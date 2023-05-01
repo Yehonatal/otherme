546101768f9c44d83caf428e502ef76f3663aab2
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-05-01 09:18:09 +0300

    js refactored , added a class 'theme-btn' to the button

--- dist/apps/js/main.js
+++ dist/apps/js/main.js
@@ -1,56 +1,20 @@
 const body = document.querySelector("body");
-
-const isDarkMode =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-// Use the color scheme variable in your code
-if (isDarkMode) {
-    body.classList.add("dark");
-    body.classList.remove("light");
-} else {
-    body.classList.add("light");
-    body.classList.remove("dark");
-}
-
-function switch_theme(btn) {
-    if (body.classList.contains("light")) {
-        body.classList.add("dark");
-        body.classList.remove("light");
-        btn.querySelector("p").innerText = "Light";
-        btn.querySelector("img").src = "assets/images/icon-sun.svg";
-    } else {
-        body.classList.add("light");
-        body.classList.remove("dark");
-        btn.querySelector("p").innerText = "Dark";
-        btn.querySelector("img").src = "assets/images/icon-moon.svg";
-    }
-}
-
-// Getting the data from the API
-const search = document.querySelector(".search__btn");
-const input = document.querySelector(".search__box");
-
-// user Information
-const avatar = document.querySelector(".user-avatar");
-const name = document.querySelector(".name");
-const login = document.querySelector(".login");
-const joined = document.querySelector(".joined-date");
-
-// Bio
-const bio = document.querySelector(".bio");
-
-// stat
-const repo = document.querySelector(".repo");
-const follower = document.querySelector(".follower");
-const following = document.querySelector(".following");
-
-// links
-const place = document.querySelector(".location");
-const twtr = document.querySelector(".twtr");
-const blog = document.querySelector(".blog");
-const work = document.querySelector(".work");
-
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const themeBtn = document.querySelector(".theme-btn");
+const searchBtn = document.querySelector(".search__btn");
+const searchBox = document.querySelector(".search__box");
+const userAvatar = document.querySelector(".user-avatar");
+const userName = document.querySelector(".name");
+const userLogin = document.querySelector(".login");
+const joinedDate = document.querySelector(".joined-date");
+const userBio = document.querySelector(".bio");
+const userRepo = document.querySelector(".repo");
+const userFollower = document.querySelector(".follower");
+const userFollowing = document.querySelector(".following");
+const userPlace = document.querySelector(".location");
+const userTwtr = document.querySelector(".twtr");
+const userBlog = document.querySelector(".blog");
+const userWork = document.querySelector(".work");
 const months = [
     "Jan",
     "Feb",
@@ -66,69 +30,60 @@ const months = [
     "Dec",
 ];
 
-search.addEventListener("click", (e) => {
+if (isDarkMode) {
+    body.classList.add("dark");
+} else {
+    body.classList.add("light");
+}
+
+themeBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    body.classList.toggle("light");
+    themeBtn.querySelector("p").innerText = body.classList.contains("light")
+        ? "Dark"
+        : "Light";
+    themeBtn.querySelector("img").src = body.classList.contains("light")
+        ? "assets/images/icon-moon.svg"
+        : "assets/images/icon-sun.svg";
+});
+
+searchBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    let userName = input.value.trim();
-    // userName = "RobiMez";
-    // userName = "R4nn3r";
+    const userName = searchBox.value.trim();
 
     if (userName.length == 0) {
         input.parentNode.classList.add("error");
         input.parentNode.querySelector("img").src =
             "assets/images/icon-search-error.svg";
-    } else {
-        input.parentNode.classList.remove("error");
+        return;
+    }
 
-        fetch(`https://api.github.com/users/${userName}`)
-            .then((response) => response.json())
-            .then((data) => {
-                userName = "";
-                avatar.src = data.avatar_url;
-                name.innerText = data.name;
+    try {
+        const response = await fetch(
+            `https://api.github.com/users/${userName}`
+        );
+        const data = await response.json();
 
-                if (data.name == "" || data.name == null) {
-                    name.innerText = "Not Available";
-                }
-                login.innerText = `@${data.login}`;
-                login.href = `https://github.com/${userName}`;
+        userAvatar.src = data.avatar_url;
+        userName.innerText = data.name || "Not Available";
+        userLogin.innerText = `@${data.login}`;
+        userLogin.href = `https://github.com/${userName}`;
 
-                let dates = data.created_at.split("T")[0].split("-");
-                let yyyy = dates[0];
-                let mm = months[dates[1] - 1];
-                let dd = dates[2];
+        const dates = data.created_at.split("T")[0].split("-");
+        const yyyy = dates[0];
+        const mm = months[dates[1] - 1];
+        const dd = dates[2];
+        joinedDate.innerText = `Joined ${dd} ${mm} ${yyyy}`;
 
-                joined.innerText = `Joined ${dd} ${mm} ${yyyy}`;
-
-                if (data.bio) {
-                    bio.innerText = data.bio;
-                }
-
-                repo.innerText = data.public_repos;
-                follower.innerText = data.followers;
-                following.innerText = data.following;
-
-                place.innerText = data.location;
-                twtr.innerText = data.twitter_username;
-                blog.innerText = data.blog;
-                work.innerText = data.company;
-
-                // Change the Text if links not available
-                if (data.location == "" || data.location == null) {
-                    place.innerText = "Not Available";
-                }
-                if (
-                    data.twitter_username == "" ||
-                    data.twitter_username == null
-                ) {
-                    twtr.innerText = "Not Available";
-                }
-                if (data.blog == "" || data.blog == null) {
-                    blog.innerText = "Not Available";
-                }
-
-                if (data.company == null || data.company == "") {
-                    work.innerText = "Not Available";
-                }
-            });
+        userBio.innerText = data.bio || "";
+        userRepo.innerText = data.public_repos;
+        userFollower.innerText = data.followers;
+        userFollowing.innerText = data.following;
+        userPlace.innerText = data.location || "Not Available";
+        userTwtr.innerText = data.twitter_username || "Not Available";
+        userBlog.innerText = data.blog || "Not Available";
+        userWork.innerText = data.company || "Not Available";
+    } catch (error) {
+        console.log(error);
     }
 });
